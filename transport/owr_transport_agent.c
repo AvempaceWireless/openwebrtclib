237e97ab239fdef31b35e9a65f7cfd08d0e97b3e
AvempaceWireless <abdelhamid.kerkeni@avempace-wireless.com>
2018-11-19 17:26:33 +0100

    - fixing issue targetting Android

--- transport/owr_transport_agent.c
+++ transport/owr_transport_agent.c
@@ -485,9 +485,14 @@ static void owr_transport_agent_init(OwrTransportAgent *transport_agent)
 
     // Avempace Code
 
+#ifdef __ANDROID__
     LOGI("owr_transport_agent_init - %s", "Abdelhamid :DISABLE ICE-TCP");
+#endif
     g_object_set(G_OBJECT(priv->nice_agent, "ice-tcp", FALSE, NULL);
+
+#ifdef __ANDROID__
     LOGI("owr_transport_agent_init - %s", "Abdelhamid :FORCE-RELAY TRUE");
+#endif
     g_object_set(G_OBJECT(priv->nice_agent, "force-relay", TRUE, NULL);
 
     /************************/
